0e840d79dab62a390e4cd3e238ca76b43ef90c7f
saaz181 <saliaz.mg326@gmail.com>
2021-01-07 18:28:30 +0330

    changing Model&template

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "core"
 
 urlpatterns = [
-    path("", views.item_list, name="item_list"),
-    path("product/", views.product, name="product"),
-    path("check_out", views.check_out, name="check_out"),
+    path("", views.HomeView.as_view(), name="home"),
+    path("product/<slug>", views.ProductDetailView.as_view(), name="product"),
+    path("checkout/", views.check_out, name="checkout"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from .models import Item
+from django.views.generic import ListView, DetailView
 
 
-def item_list(request):
+# in our template we say {% for item in objects_list %}
+class HomeView(ListView):
+    model = Item
+    template_name = "core/home-page.html"
+
+class ProductDetailView(DetailView):
+    model = Item
+    template_name = "core/product-page.html"
+
+
+
+
+''' def home(request):
     context = {
         'items': Item.objects.all()
     }
     return render(request, "core/home-page.html", context)
+ '''
 
-
-def product(request):
+''' def product(request):
     return render(request, 'core/product-page.html', context={})
-
+ '''
 
 def check_out(request):
     return render(request, 'core/checkout-page.html', context={})
